9694e6abdce2047ed3e5e1fb54a2e7bc5157b8db
samcgibson <samcg@umich.edu>
2023-04-08 17:44:56 -0400

    finished find_rest_in_building()

--- HW8.py
+++ HW8.py
@@ -31,11 +31,7 @@ def load_rest_data(db):
     return ndict
 
 def plot_rest_categories(db):
-    """
-    This function accepts a file name of a database as a parameter and returns a dictionary. The keys should be the
-    restaurant categories and the values should be the number of restaurants in each category. The function should
-    also create a bar chart with restaurant categories and the count of number of restaurants in each category.
-    """
+
     path = os.path.dirname(os.path.abspath(__file__))
     conn = sqlite3.connect(path+'/'+db)
     cur = conn.cursor()
@@ -60,16 +56,20 @@ def plot_rest_categories(db):
     plt.xticks([0, 1, 2, 3, 4])
     plt.tight_layout()
     plt.show()
-    print(sdict)
     return sdict
 
 def find_rest_in_building(building_num, db):
-    '''
-    This function accepts the building number and the filename of the database as parameters and returns a list of 
-    restaurant names. You need to find all the restaurant names which are in the specific building. The restaurants 
-    should be sorted by their rating from highest to lowest.
-    '''
-    pass
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db)
+    cur = conn.cursor()
+
+    cur.execute(f"SELECT restaurants.name, buildings.id, restaurants.rating FROM restaurants JOIN buildings ON restaurants.building_id = buildings.id WHERE buildings.building = {building_num} ORDER by restaurants.rating DESC")
+
+    rlist = []
+    for row in cur:
+        rlist.append(row[0])
+
+    return rlist
 
 #EXTRA CREDIT
 def get_highest_rating(db): #Do this through DB as well
@@ -87,54 +87,54 @@ def get_highest_rating(db): #Do this through DB as well
 
 #Try calling your functions here
 def main():
-    plot_rest_categories('South_U_Restaurants.db')
+    pass
 
-# class TestHW8(unittest.TestCase):
-#     def setUp(self):
-#         self.rest_dict = {
-#             'category': 'Cafe',
-#             'building': 1101,
-#             'rating': 3.8
-#         }
-#         self.cat_dict = {
-#             'Asian Cuisine ': 2,
-#             'Bar': 4,
-#             'Bubble Tea Shop': 2,
-#             'Cafe': 3,
-#             'Cookie Shop': 1,
-#             'Deli': 1,
-#             'Japanese Restaurant': 1,
-#             'Juice Shop': 1,
-#             'Korean Restaurant': 2,
-#             'Mediterranean Restaurant': 1,
-#             'Mexican Restaurant': 2,
-#             'Pizzeria': 2,
-#             'Sandwich Shop': 2,
-#             'Thai Restaurant': 1
-#         }
-#         self.highest_rating = [('Deli', 4.6), (1335, 4.8)]
+class TestHW8(unittest.TestCase):
+    def setUp(self):
+        self.rest_dict = {
+            'category': 'Cafe',
+            'building': 1101,
+            'rating': 3.8
+        }
+        self.cat_dict = {
+            'Asian Cuisine ': 2,
+            'Bar': 4,
+            'Bubble Tea Shop': 2,
+            'Cafe': 3,
+            'Cookie Shop': 1,
+            'Deli': 1,
+            'Japanese Restaurant': 1,
+            'Juice Shop': 1,
+            'Korean Restaurant': 2,
+            'Mediterranean Restaurant': 1,
+            'Mexican Restaurant': 2,
+            'Pizzeria': 2,
+            'Sandwich Shop': 2,
+            'Thai Restaurant': 1
+        }
+        self.highest_rating = [('Deli', 4.6), (1335, 4.8)]
 
-#     def test_load_rest_data(self):
-#         rest_data = load_rest_data('South_U_Restaurants.db')
-#         self.assertIsInstance(rest_data, dict)
-#         self.assertEqual(rest_data['M-36 Coffee Roasters Cafe'], self.rest_dict)
-#         self.assertEqual(len(rest_data), 25)
+    def test_load_rest_data(self):
+        rest_data = load_rest_data('South_U_Restaurants.db')
+        self.assertIsInstance(rest_data, dict)
+        self.assertEqual(rest_data['M-36 Coffee Roasters Cafe'], self.rest_dict)
+        self.assertEqual(len(rest_data), 25)
 
-#     def test_plot_rest_categories(self):
-#         cat_data = plot_rest_categories('South_U_Restaurants.db')
-#         self.assertIsInstance(cat_data, dict)
-#         self.assertEqual(cat_data, self.cat_dict)
-#         self.assertEqual(len(cat_data), 14)
+    def test_plot_rest_categories(self):
+        cat_data = plot_rest_categories('South_U_Restaurants.db')
+        self.assertIsInstance(cat_data, dict)
+        self.assertEqual(cat_data, self.cat_dict)
+        self.assertEqual(len(cat_data), 14)
 
-#     def test_find_rest_in_building(self):
-#         restaurant_list = find_rest_in_building(1140, 'South_U_Restaurants.db')
-#         self.assertIsInstance(restaurant_list, list)
-#         self.assertEqual(len(restaurant_list), 3)
-#         self.assertEqual(restaurant_list[0], 'BTB Burrito')
+    def test_find_rest_in_building(self):
+        restaurant_list = find_rest_in_building(1140, 'South_U_Restaurants.db')
+        self.assertIsInstance(restaurant_list, list)
+        self.assertEqual(len(restaurant_list), 3)
+        self.assertEqual(restaurant_list[0], 'BTB Burrito')
 
-#     def test_get_highest_rating(self):
-#         highest_rating = get_highest_rating('South_U_Restaurants.db')
-#         self.assertEqual(highest_rating, self.highest_rating)
+    # def test_get_highest_rating(self):
+    #     highest_rating = get_highest_rating('South_U_Restaurants.db')
+    #     self.assertEqual(highest_rating, self.highest_rating)
 
 if __name__ == '__main__':
     main()
